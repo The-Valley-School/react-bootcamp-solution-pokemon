2f52edae4cf10c602fc1b6a6b3661b30530806a1
Fran Linde Blazquez <franlindebl@gmail.com>
2023-03-17 14:00:27 +0100

    Added react-intl

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,32 @@
 import "./Header.scss";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import pokemonLogo from "../../assets/pokemon-logo.png";
+import { FaArrowLeft } from "react-icons/fa";
+import { LanguageSelector } from "../../App";
+import { useContext } from "react";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const { setLanguage } = useContext(LanguageSelector);
+
   return (
     <header className="header">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/pokemons">Pokemons</NavLink>
-      <NavLink to="/game">Game</NavLink>
+      <button className="header__go-back" onClick={() => navigate(-1)}>
+        <FaArrowLeft />
+      </button>
+
+      <NavLink to="/" className="header__link">
+        <img className="header__logo-image" src={pokemonLogo} />
+      </NavLink>
+
+      <div className="header__lang">
+        <button onClick={() => setLanguage("es-ES")} className="header__lang-btn">
+          ES
+        </button>
+        <button onClick={() => setLanguage("en-EN")} className="header__lang-btn">
+          EN
+        </button>
+      </div>
     </header>
   );
 };

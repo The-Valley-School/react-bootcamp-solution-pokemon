1e7d50b6bafe14bd25c314272bfc1d091f548a9a
Fran Linde Blazquez <franlindebl@gmail.com>
2023-03-17 17:20:32 +0100

    Finished home and footer

--- src/pages/GamePage/GamePage.js
+++ src/pages/GamePage/GamePage.js
@@ -1,11 +1,13 @@
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import "./GamePage.scss";
 
 const GamePage = () => {
   return (
-    <div className="game-page">
+    <div className="game-page page">
       <Header></Header>
-      <p>GamePage!</p>
+      <div className="game-page__content page__content"></div>
+      <Footer></Footer>
     </div>
   );
 };

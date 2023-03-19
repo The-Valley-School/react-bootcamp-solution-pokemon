5d15fb55d9a4cc50157734e5ab64f8918893a380
Fran Linde Blazquez <franlindebl@gmail.com>
2023-03-19 17:27:57 +0100

    Added some styles on pokemon main info

--- src/pages/PokemonDetailPage/PokemonMainInfo/PokemonMainInfo.js
+++ src/pages/PokemonDetailPage/PokemonMainInfo/PokemonMainInfo.js
@@ -1,7 +1,29 @@
+import { FormattedMessage } from "react-intl";
+import "./PokemonMainInfo.scss";
+
 const PokemonMainInfo = ({ pokemonData }) => {
   return (
     <div className="pokemon-main-info">
-      <div className="pokemon-main-info__name">{pokemonData?.name}</div>
+      <img className="pokemon-main-info__image" src={pokemonData?.sprites?.other?.["official-artwork"]?.front_default} />
+      <p className="pokemon-main-info__number">#{pokemonData?.id || ""}</p>
+      <p className="pokemon-main-info__name">{pokemonData?.name || "- -"}</p>
+      <div className="pokemon-main-info__types">
+        {pokemonData?.types.map((item) => (
+          <span className={`btn btn--small pokemon-main-info__type background-${item.type.name}`} key={item.type.name}>
+            {item.type.name}
+          </span>
+        ))}
+      </div>
+      <div className="pokemon-main-info__data">
+        <span className="pokemon-main-info__attr">
+          <FormattedMessage id="pokemons:weight" />:
+        </span>{" "}
+        {pokemonData?.weight}KG
+        <span className="pokemon-main-info__attr">
+          <FormattedMessage id="pokemons:height" />:
+        </span>{" "}
+        {pokemonData?.height}M
+      </div>
     </div>
   );
 };
--- src/pages/PokemonDetailPage/PokemonStats/PokemonStats.js
+++ src/pages/PokemonDetailPage/PokemonStats/PokemonStats.js
@@ -1,5 +1,23 @@
-const PokemonStats = () => {
-  return <div className="pokemon-stats">pokemon-stats</div>;
+import { FormattedMessage } from "react-intl";
+import "./PokemonStats.scss";
+
+const PokemonStats = ({ stats }) => {
+  return (
+    <div className="pokemon-stats">
+      <h3 className="pokemon-stats__title">
+        <FormattedMessage id="pokemon-detail:stats" />
+      </h3>
+      <div className="pokemon-stats__stats">
+        {stats?.map((item) => (
+          <div className="pokemon-stats__data-row" key={item.stat.name}>
+            <span className="pokemon-stats__data-name">{item.stat.name}</span>
+            <span className="pokemon-stats__data-value">{item.base_stat}</span>
+            <span>PROGRESS</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PokemonStats;

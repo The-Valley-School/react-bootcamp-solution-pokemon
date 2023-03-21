95d4c8814ef3af8cfc35f058f01942563d1784ed
Fran Linde Blazquez <franlindebl@gmail.com>
2023-03-21 16:13:52 +0100

    Added some simple tests

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,7 +3,7 @@ import "./ProgressBar.scss";
 const ProgressBar = ({ value }) => {
   return (
     <div className="progress-bar">
-      <div className="progress-bar__inner" style={{ width: `${value}%` }}></div>
+      <div data-testid="progress-bar__inner" className="progress-bar__inner" style={{ width: `${value}%` }}></div>
     </div>
   );
 };
